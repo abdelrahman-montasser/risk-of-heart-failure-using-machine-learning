b4cdbf68090d9d775aa0c5e14e3f2f89320a41f3
Abdelrahman Montasser <amontsser@gmail.com>
2021-09-11 23:18:59 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+#import libraries
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+#Initialize the flask App
+app = Flask(__name__)
+model = pickle.load(open('heart_failure.pickle', 'rb'))
+
+#default page of our web-app
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#To use the predict button in our web-app
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Risk of heart failure (0 for low risk,1 for high risk) :{}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
